819da1a46aceae1c6684c9f405d404a5a68e33ef
Abdul <sanikhusravi@gmail.com>
2022-04-06 02:03:03 -0500

    cartManagement 1.0 version

---
+++ src/main/java/com/training/CartManagment/CartManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.training.CartManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/CartManagment/CartManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.CartManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
